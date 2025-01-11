83cfe0ba746f2940822adb0615f95f4fb887e7d1
tomandrews007 <93524253+tomandrews007@users.noreply.github.com>
2025-01-11 16:18:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import App from './App'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
-    </Router>
+    </ChakraProvider>
   </React.StrictMode>
-);
+)
