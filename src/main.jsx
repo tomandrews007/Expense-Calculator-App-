a5493995f356c370a0a47cfca6fbcaebd76b6944
tomandrews007 <93524253+tomandrews007@users.noreply.github.com>
2025-01-11 15:46:49 +0530

    Update main.jsx
    Switch from BrowserRouter to HashRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App'
-import theme from './theme'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <Router>
       <App />
-    </ChakraProvider>
+    </Router>
   </React.StrictMode>
-)
+);
